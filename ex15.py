5572c148f763f16187ea048e245865e35c2aba3f
Hugo1030 <yzhjsw520@gmail.com>
2017-08-10 15:29:30 +0800

    jupter magic ran and time

--- ex15.py
+++ ex15.py
@@ -4,13 +4,13 @@ script, filename = argv
 
 txt = open(filename)
 
-print "Here's your file %r:" % filename
+print ("Here's your file %r:" % filename)
 print txt.read()
 
-print "Type the filename again:"
+print ("Type the filename again:")
 file_again = raw_input("> ")
 
 txt_again = open(file_again)
 
-print txt_again.read()
+print (txt_again.read())
 txt.close()
